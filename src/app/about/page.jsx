39048e863b29e7eb5ec9049b8906138a5e4fd942
StalicJi <a7330831@gmail.com>
2024-03-23 00:52:00 +0800

    create aboutpage

---
+++ src/app/about/page.jsx
@@ -0,0 +1,34 @@
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur aut,
+        ullam odio dignissimos iste fugiat nihil, accusantium qui exercitationem
+        eius atque consequatur laudantium. Inventore accusamus est temporibus.
+        Ex, aut consectetur. Lorem ipsum dolor, sit amet consectetur adipisicing
+        elit. Pariatur aut, ullam odio dignissimos iste fugiat nihil
+      </p>
+      <p>
+        accusantium qui exercitationem eius atque consequatur laudantium.
+        Inventore accusamus est temporibus. Ex, aut consectetur. Lorem ipsum
+        dolor, sit amet consectetur adipisicing elit. Pariatur aut, ullam odio
+        dignissimos iste fugiat nihil, accusantium qui exercitationem eius atque
+        consequatur laudantium. Inventore accusamus est temporibus. Ex, aut
+        consectetur. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Pariatur aut, ullam odio dignissimos iste fugiat nihil, accusantium qui
+        exercitationem eius atque consequatur laudantium. Inventore accusamus
+        est temporibus. Ex, aut consectetur. Lorem ipsum dolor, sit amet
+      </p>
+      <p>
+        consectetur adipisicing elit. Pariatur aut, ullam odio dignissimos iste
+        fugiat nihil, accusantium qui exercitationem eius atque consequatur
+        laudantium. Inventore accusamus est temporibus. Ex, aut consectetur.
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur aut,
+        ullam odio dignissimos iste fugiat nihil, accusantium qui exercitationem
+        eius atque consequatur laudantium. Inventore accusamus est temporibus.
+        Ex, aut consectetur.
+      </p>
+    </div>
+  );
+}
