4b8fc54964448d55b8accc2b66033007e8480162
StalicJi <a7330831@gmail.com>
2024-03-24 18:38:05 +0800

    finish

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,9 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "../components/Header";
+import Header from "@/components/Header";
+import Navbar from "@/components/Navbar";
 import Providers from "./Providers";
+import SearchBox from "@/components/SearchBox";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +18,8 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Providers>
           <Header />
+          <Navbar />
+          <SearchBox />
           {children}
         </Providers>
       </body>
